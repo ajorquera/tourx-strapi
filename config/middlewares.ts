96c90b224fc9953cda28e194c423851b3d017a64
andres <jorquera.ad@gmail.com>
2025-11-22 17:52:25 +0100

    Setting supabase

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,32 @@
-export default [
+export default ({env}) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "default-src": ["'self'"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            env("SUPABASE_API_URL"),
+          ],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      pool_mode: 'session',
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,23 @@
-export default () => ({
+export default ({env}) => ({
   seo: {
     enabled: true,
-  }
+  },
+  upload: {
+    config: {
+      provider: "strapi-upload-supabase-provider",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET_NAME"),
+        directory: env("SUPABASE_BUCKET_DIRECTORY"),
+      },
+      sizeLimit: 1000000000,
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+        checkFileSize: {},
+      },
+    },
+  },
 });
