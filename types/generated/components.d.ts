bcebae1aba3d62fd01c673387f84b9716665295e
andres <jorquera.ad@gmail.com>
2026-03-24 20:14:24 +0100

    Update schema

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,116 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface ScheduleCustomDates extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_custom_dates';
+  info: {
+    displayName: 'CustomDates';
+  };
+  attributes: {
+    dates: Schema.Attribute.Component<'shared.dates', true>;
+    times: Schema.Attribute.Component<'shared.times', true>;
+  };
+}
+
+export interface ScheduleCustomMonths extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_custom_months';
+  info: {
+    displayName: 'CustomMonths';
+  };
+  attributes: {
+    days: Schema.Attribute.Component<'schedule.days', false>;
+    months: Schema.Attribute.Component<'schedule.months', false>;
+    times: Schema.Attribute.Component<'shared.times', true>;
+  };
+}
+
+export interface ScheduleDateRange extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_date_ranges';
+  info: {
+    displayName: 'DateRange';
+  };
+  attributes: {
+    dates: Schema.Attribute.Component<'shared.dates', true>;
+    days: Schema.Attribute.Component<'schedule.days', false>;
+    end: Schema.Attribute.Date;
+    start: Schema.Attribute.Date;
+  };
+}
+
+export interface ScheduleDays extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_days';
+  info: {
+    displayName: 'Days';
+  };
+  attributes: {
+    Friday: Schema.Attribute.Boolean;
+    Monday: Schema.Attribute.Boolean;
+    Saturday: Schema.Attribute.Boolean;
+    Sunday: Schema.Attribute.Boolean;
+    Thursday: Schema.Attribute.Boolean;
+    Tuesday: Schema.Attribute.Boolean;
+    Wednesday: Schema.Attribute.Boolean;
+  };
+}
+
+export interface ScheduleMonths extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_months';
+  info: {
+    displayName: 'Months';
+  };
+  attributes: {
+    April: Schema.Attribute.Boolean;
+    August: Schema.Attribute.Boolean;
+    December: Schema.Attribute.Boolean;
+    February: Schema.Attribute.Boolean;
+    January: Schema.Attribute.Boolean;
+    July: Schema.Attribute.Boolean;
+    June: Schema.Attribute.Boolean;
+    March: Schema.Attribute.Boolean;
+    May: Schema.Attribute.Boolean;
+    November: Schema.Attribute.Boolean;
+    October: Schema.Attribute.Boolean;
+    September: Schema.Attribute.Boolean;
+  };
+}
+
+export interface ScheduleSchedule extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_schedules';
+  info: {
+    displayName: 'Schedule';
+  };
+  attributes: {
+    customDates: Schema.Attribute.Component<'schedule.custom-dates', false>;
+    customMonths: Schema.Attribute.Component<'schedule.custom-months', false>;
+    dateRange: Schema.Attribute.Component<'schedule.date-range', false>;
+    name: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<
+      ['Date Range', 'Custom Dates', 'Yearly', 'Custom Months']
+    >;
+    yearly: Schema.Attribute.Component<'schedule.yearly', false>;
+  };
+}
+
+export interface ScheduleYearly extends Struct.ComponentSchema {
+  collectionName: 'components_schedule_yearlies';
+  info: {
+    displayName: 'Yearly';
+  };
+  attributes: {
+    days: Schema.Attribute.Component<'schedule.days', false>;
+    times: Schema.Attribute.Component<'shared.times', true>;
+  };
+}
+
+export interface SharedDates extends Struct.ComponentSchema {
+  collectionName: 'components_shared_dates';
+  info: {
+    displayName: 'Dates';
+  };
+  attributes: {
+    Date: Schema.Attribute.Date;
+  };
+}
+
 export interface SharedDuration extends Struct.ComponentSchema {
   collectionName: 'components_shared_durations';
   info: {
@@ -91,6 +202,16 @@ export interface SharedSeo extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedTimes extends Struct.ComponentSchema {
+  collectionName: 'components_shared_times';
+  info: {
+    displayName: 'Times';
+  };
+  attributes: {
+    time: Schema.Attribute.Time;
+  };
+}
+
 export interface SharedVideo extends Struct.ComponentSchema {
   collectionName: 'components_shared_videos';
   info: {
@@ -145,12 +266,21 @@ export interface SliderSlider extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'schedule.custom-dates': ScheduleCustomDates;
+      'schedule.custom-months': ScheduleCustomMonths;
+      'schedule.date-range': ScheduleDateRange;
+      'schedule.days': ScheduleDays;
+      'schedule.months': ScheduleMonths;
+      'schedule.schedule': ScheduleSchedule;
+      'schedule.yearly': ScheduleYearly;
+      'shared.dates': SharedDates;
       'shared.duration': SharedDuration;
       'shared.languages': SharedLanguages;
       'shared.link': SharedLink;
       'shared.nav': SharedNav;
       'shared.open-graph': SharedOpenGraph;
       'shared.seo': SharedSeo;
+      'shared.times': SharedTimes;
       'shared.video': SharedVideo;
       'slider.card-slider': SliderCardSlider;
       'slider.slide': SliderSlide;
